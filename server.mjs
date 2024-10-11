a1e457b7557fd5050336298d1fcef539374838af
Antman295 <antjosh14@gmail.com>
2024-10-11 15:38:29 -0400

    Update server.mjs
    Added some Error Handling and I think it works...?

--- server.mjs
+++ server.mjs
@@ -3,8 +3,6 @@ import express from "express";
 import footballPlayerRoutes from './routes/footballPlayerRoutes.mjs';
 import bodyParser from "body-parser";
 
-// Data import
-import { players } from './data/footballPlayers.mjs'
 
 // Instance of express
 const app = express();
@@ -17,12 +15,19 @@ app.use(bodyParser.json({extended: true}))
 // Routes
 app.use('/api/players', footballPlayerRoutes); // Route for players data
 
-// Error Handling
+// Middleware Error Handling
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ error: err.message });
+});
 
+app.use((req, res, next) => {
+    next(error(404, "Data not found in database!"))
+})
 
 // Catch all route for incorrect inputs
 app.get('*', (req, res) => {
-    res.status(404).send('Data not found in database!')
+    res.status(404).send('Invalid link!')
 })
 
 // Listen
