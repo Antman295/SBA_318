3acfea3163d0c7878f32ccc73329eac9e5d9eb46
Antman295 <antjosh14@gmail.com>
2024-10-13 18:19:23 -0400

    HTML Views Update
    Created views on my server.mjs file with a "fs" view template engine. Also began working on the HTML for said view templates

--- server.mjs
+++ server.mjs
@@ -5,6 +5,7 @@ import restaurantRoutes from './routes/restaurantRoutes.mjs';
 import reviewRoutes from './routes/reviewRoutes.mjs';
 import bodyParser from "body-parser";
 import error from './utilities/error.mjs'
+import fs from 'fs';
 
 
 // Instance of express
@@ -12,9 +13,45 @@ const app = express();
 let PORT = 3000;
 
 // Middleware
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json({extended: true}))
 
+// Serving Static Files 
+app.use(express.static('./styles'));
+
+// Body Parsing
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json({extended: true}));
+
+// View Engine
+app.engine('file', (filePath, options, callback) => {
+    fs.readFile(filePath, (err, content) => {
+        if (err) return callback(err);
+
+        if (options.allPlayers) {
+            let result = '';
+
+            options.allPlayers.forEach((el) => {
+                result += `<h2>Name: ${el.name}<h2><h3>Species: ${el.species}</h3><h3>Age: ${el.age}</h3><a href="/api/animal/${el.id}"><button>More Info</button></a><br><br>`;
+            });
+
+            const rendered = content.toString().replace('#content#', result);
+
+            return callback(null, rendered);
+        } else {
+            const rendered = content
+                .toString()
+                .replaceAll('#name#', `${options.name}`)
+                .replace('#position#', `${options.position}`)
+                .replace('#team#', `${options.team}`)
+                .replace('#players#', options.player);
+
+            return callback(null, rendered);
+        }
+        
+    });
+});
+
+app.set('views', './views'); // Specifies the views directory
+app.set('view engine', 'file'); // Registers the template engine
 // Routes
 app.use('/api/players', footballPlayerRoutes); // Route for players data
 app.use('/api/restaurants', restaurantRoutes); // Route for restaurant data
